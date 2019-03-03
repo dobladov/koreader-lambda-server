2e8f395b309f7c62633e3291497485db35f78292
Daniel Doblado <danieldoblado@gmail.com>
2019-03-04 00:35:33 +0100

    Update route for database

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('db.json')
+const adapter = new FileSync('/tmp/db.json')
 
 const db = low(adapter)
 db.defaults({ documents: [], users: [] }).write()
